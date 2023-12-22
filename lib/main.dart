60a3a7860c1fc4981d771583b58ea98536bd190a
MyintThanTun <myintthantunn2020@gmail.com>
2023-12-22 20:14:45 +0630

    "wrap"

--- lib/main.dart
+++ lib/main.dart
@@ -48,90 +48,32 @@ class MyApp extends StatelessWidget {
           title: const Text('My Application'),
           centerTitle: false,
         ),
-        // body: Center(
-        //   child: Container(
-        //     margin: const EdgeInsets.only(left: 30),
-        //     alignment: Alignment.center,
-        //     constraints: const BoxConstraints(
-        //         maxHeight: 200, maxWidth: 200, minHeight: 200, minWidth: 200),
-        //     decoration: BoxDecoration(
-        //         gradient: const LinearGradient(
-        //             colors: [
-        //               Colors.greenAccent,
-        //               Colors.indigoAccent,
-        //               Colors.orangeAccent
-        //             ],
-        //             begin: Alignment.topLeft,
-        //             end: Alignment.bottomRight,
-        //             stops: [0.5, 0.4, 0.3]),
-        //         borderRadius: BorderRadius.circular(20),
-        //         shape: BoxShape.rectangle,
-        //         color: Colors.black,
-        //         border: Border.all(color: Colors.redAccent, width: 6)),
-        //     child: const Text(
-        //       'Container',
-        //       style: TextStyle(
-        //           fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
-        //       overflow: TextOverflow.ellipsis,
-        //     ),
-        //   ),
-        // ),
-        body: Center(
-          child: Stack(
-            clipBehavior: Clip.none,
-            alignment: AlignmentDirectional.topCenter,
+        body: Container(
+          height: 500,
+          width: 500,
+          color: Colors.red,
+          child: Wrap(
+            direction: Axis.horizontal,
+            crossAxisAlignment: WrapCrossAlignment.center,
+            spacing: 20,
+            runSpacing: 20,
+            alignment: WrapAlignment.spaceBetween,
             children: [
-              Positioned(
-                child: Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.black),
-                  height: 400,
-                  width: 400,
-                  child: const Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Myint Than Tun',
-                          style: TextStyle(color: Colors.white),
-                          textAlign: TextAlign.center,
-                        ),
-                        Text(
-                          'Age - 18',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        Icon(Icons.boy),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              Positioned(
-                top: -75,
-                left: 120,
-                child: Container(
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        image:
-                            AssetImage('images/photo_2023-12-05_13-14-40.jpg'),
-                        fit: BoxFit.contain),
-                    shape: BoxShape.circle,
-                    color: Colors.yellowAccent,
-                  ),
-                  width: 150,
-                  height: 150,
-                ),
-              ),
-              // Positioned(
-              //   left: 0,
-              //   bottom: 0,
-              //   child: Container(
-              //     color: Colors.blue,
-              //     width: 100,
-              //     height: 100,
-              //   ),
-              // )
+              ElevatedButton(onPressed: () {}, child: const Text('Hello 1')),
+              ElevatedButton(onPressed: () {}, child: const Text('Hello 2')),
+              ElevatedButton(onPressed: () {}, child: const Text('Hello 3')),
+              ElevatedButton(onPressed: () {}, child: const Text('Hello 4')),
+              ElevatedButton(onPressed: () {}, child: const Text('Hello 5')),
+              ElevatedButton(onPressed: () {}, child: const Text('Hello 6')),
+              ElevatedButton(onPressed: () {}, child: const Text('Hello 7')),
+              ElevatedButton(onPressed: () {}, child: const Text('Hello 8')),
+              ElevatedButton(onPressed: () {}, child: const Text('Hello 9')),
+              ElevatedButton(onPressed: () {}, child: const Text('Hello 10')),
+              ElevatedButton(onPressed: () {}, child: const Text('Hello 11')),
+              ElevatedButton(onPressed: () {}, child: const Text('Hello 12')),
+              ElevatedButton(onPressed: () {}, child: const Text('Hello 13')),
+              ElevatedButton(onPressed: () {}, child: const Text('Hello 13')),
+              ElevatedButton(onPressed: () {}, child: const Text('Hello 13')),
             ],
           ),
         ));
